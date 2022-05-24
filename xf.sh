916775fa54e780c3d52d6e1734497d5afb374bb4
hisgj <70511926+hisgj@users.noreply.github.com>
2022-05-24 15:40:31 +0800

    Update xf.sh

--- xf.sh
+++ xf.sh
@@ -60,7 +60,7 @@ cat <<EOF >/etc/v2ray/config.json
             "settings": {
                 "clients": [
                     {
-                        "id": "11111111-1111-1111-1111-111111111111",
+                        "id": "a55b0e2b-8843-41c4-9149-65c4bb314314",
                         "alterId": 0
                     }
                 ],
